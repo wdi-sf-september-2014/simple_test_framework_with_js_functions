1fc8f95485d8a9e6ab34b5ecb4659eef12434b15
Alex Notov <anotov@gmail.com>
2014-09-30 17:29:10 -0700

    add the beginnings of the test suite

---
+++ test_suite.js
@@ -0,0 +1,13 @@
+T.describe("Basic js operators", function() {
+  T.describe("Addition", function() {
+    T.it("should equal to 4 when adding 2 + 2", function() {
+      return 2+2 === 5;
+    });  
+  });
+  
+  T.describe("Subtraction", function() {
+    T.it("should equal to 4 when subtracting 10 - 6", function() {
+      return 10 - 6 === 4;
+    });      
+  });  
+});
