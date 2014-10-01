925c142abf596532064e6f62d4dc4011a6ffab0a
Alex Notov <anotov@gmail.com>
2014-09-30 21:53:50 -0700

    solution to homework

---
+++ test_framework.js
@@ -0,0 +1,43 @@
+var Describe = function(title) {
+  this.title = title;
+};
+
+var It = function(title, parent) {
+  this.title = title;
+  this.parent = parent;
+};
+
+exports.framework = {
+  nodes: [],
+
+  /**
+   * Calls the function passed to it, containing nested it examples
+   * @param {string} title - title of this describe
+   * @param {Function} fn - function for nested it examples
+   */
+  describe: function(title, fn) {
+    var describe = new Describe(title);
+    this.nodes.unshift(describe);
+    fn.call(describe);
+    this.nodes.shift();
+  },
+  
+  /**
+   * Calls the function passed to it, containing a single
+   * assertion, which is the body of the test, and returns
+   * true or false specifying the result of the assertion
+   * @param {string} title - title of this example
+   * @param {Function} fn - function for the example
+   */
+  it: function(title, fn) {
+    var parent = this.nodes[0];
+    var it = new It(title, parent);
+    try {
+      console.assert(fn.call(it), parent.title + ' ' + it.title);
+      console.log('*');
+    } catch (ex) {
+      console.error(ex);
+    } 
+  }
+  
+};
